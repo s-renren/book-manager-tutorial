9dcad08095d0bd3e32934ebbc13c00dcbedfe420
s-renren <s1f102301156@iniad.org>
2025-03-25 16:48:15 +0900

    getの修正 クエリにキーワードを追加すると、キーワードがnameに含まれる本のみを返すようにする

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,39 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type BookManager = {
+  id: number;
+  name: string;
+  status: string;
+};
+
+const bookManager: BookManager[] = [
+  { id: 1, name: "React入門", status: "在庫あり" },
+  { id: 2, name: "TypeScript入門", status: "貸出中" },
+  { id: 3, name: "Next.js入門", status: "返却済" },
+];
+
+app.get("/books", async (c) => {
+  const query = c.req.query()
+  const keyword = query.keyword
+
+  if(keyword) {
+    return c.json(bookManager.filter((book) => book.name.includes(keyword)))
+  }
+
+  return c.json(bookManager);
+});
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 8080;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  port,
+});
