ad4dbb2869a46fd6b4034d6348ee567c7687b45d
s-renren <s1f102301156@iniad.org>
2025-03-25 16:58:32 +0900

    postを作成

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,14 +16,32 @@ const bookManager: BookManager[] = [
 ];
 
 app.get("/books", async (c) => {
-  const query = c.req.query()
-  const keyword = query.keyword
+  const query = c.req.query();
+  const keyword = query.keyword;
 
-  if(keyword) {
-    return c.json(bookManager.filter((book) => book.name.includes(keyword)))
+  if (keyword) {
+    return c.json(bookManager.filter((book) => book.name.includes(keyword)));
   }
 
   return c.json(bookManager);
+});
+
+app.post("/books", async (c) => {
+  const body = await c.req.json();
+  const name = body.name;
+
+  if (name === "") {
+    return c.json({ error: "書籍名は必須です" });
+  }
+
+  const newBook = {
+    id: bookManager.length + 1,
+    name,
+    status: "在庫あり",
+  };
+
+  bookManager.push(newBook);
+  return c.json(newBook);
 });
 
 app.get("/", (c) => {
